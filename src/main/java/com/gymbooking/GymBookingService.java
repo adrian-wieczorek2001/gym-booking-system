b652af5449713a4eb8c6114038708f0965b2a9b8
Adrian Wieczorek <adrian.wieczorek2001@proton.me>
2026-07-16 11:53:47 +0200

    feat: add GymBookingService class with ArrayList of Reservations

---
+++ src/main/java/com/gymbooking/GymBookingService.java
@@ -0,0 +1,22 @@
+package com.gymbooking;
+
+import java.util.ArrayList;
+
+public class GymBookingService {
+    private ArrayList<Reservation> reservations;
+
+    public GymBookingService() {
+        this.reservations = new ArrayList<>();
+    }
+
+    public void addReservation(Reservation reservation) {
+        reservations.add(reservation);
+    }
+
+    public ArrayList<Reservation> getAllReservations() {
+        return reservations;
+    }
+
+
+
+}
--- src/main/java/com/gymbooking/Main.java
+++ src/main/java/com/gymbooking/Main.java
@@ -1,16 +1,29 @@
 package com.gymbooking;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
 
-        Member member = new Member("Adrian", 24, null);
-        Reservation reservation = new Reservation(1, member, "15.07.2026");
+        Member member1 = new Member("Adrian", 24, null);
+        Member member2 = new Member("Bartosz", 22, null);
+        Reservation reservation1 = new Reservation(1, member1, "15.07.2026");
+        Reservation reservation2 = new Reservation(2, member2, "16.07.2026");
 
 
-        System.out.println(reservation.getMember().getName());
+        System.out.println(reservation1.getMember().getName());
 
 
+        GymBookingService reservations = new GymBookingService();
 
+        reservations.addReservation(reservation1);
+        reservations.addReservation(reservation2);
+
+        for (Reservation reservation : reservations.getAllReservations()) {
+            System.out.println(reservation.getId());
+            System.out.println(reservation.getMember().getName());
+            System.out.println(reservation.getDateTime());
+        }
 
     }
 }
