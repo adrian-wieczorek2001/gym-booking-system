d6870c32a60348fc57eee5b7e41b9e4ca388c28d
Adrian Wieczorek <adrian.wieczorek2001@proton.me>
2026-08-28 16:26:37 +0200

    chore: remove unused HashSet import

--- src/main/java/com/gymbooking/GymBookingService.java
+++ src/main/java/com/gymbooking/GymBookingService.java
@@ -3,7 +3,6 @@ package com.gymbooking;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
 
 public class GymBookingService {
